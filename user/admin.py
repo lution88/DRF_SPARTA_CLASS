99d7ca018ba09be1924973122ef9fde206d32fcc
lution88 <lution88@gmail.com>
2022-06-14 09:41:29 +0900

    add CBV  UserApiView

--- user/admin.py
+++ user/admin.py
@@ -5,11 +5,14 @@ from django.contrib.auth.models import Group
 
 from user.models import Hobby as HobbyModel, UserProfile as UserProfileModel, User as UserModel
 
+
 # admin 꾸미기
-class Hobbyadmin(admin.ModelAdmin):
+class HobbyAdmin(admin.ModelAdmin):
+    # admin 페이지에서 보여주는 필드들.
     list_display = ('id', 'name')
 
-admin.site.register(HobbyModel, Hobbyadmin)
+
+admin.site.register(HobbyModel, HobbyAdmin)
 admin.site.register(UserProfileModel)
 admin.site.register(UserModel)
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+
 # Create your views here.
+from user.models import Hobby
+
+
+class MyGoodPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        result = bool(user and user.is_authenticated and user.permissions_rank > 5)
+        return result
+
+
+class UserApiView(APIView):
+    permission_classes = [permissions.AllowAny]
+    # permission_classes = [MyGoodPermission]
+
+    def get(self, request):
+        # Hobby 조회
+        hobby_list = Hobby.objects.filter(id__gt=5)
+        print(hobby_list)
+
+        return Response({"message": "get success!!"})
