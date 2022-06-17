a47da8a0622482dcc311a6830ffd1d152a9f7dcc
lution88 <lution88@gmail.com>
2022-06-17 11:25:11 +0900

    views.py class UserApiView의 get, post 메소드 생성

--- user/models.py
+++ user/models.py
@@ -25,30 +25,28 @@ class UserManager(BaseUserManager):
         return user
 
 
+# 이름만 유저 models.Model , AbstractBaseUser 커스텀 유저모델 : 장고에 등록한 User 변경.
 class User(AbstractBaseUser):
     username = models.CharField("사용자 계정", max_length=20, unique=True)
     email = models.EmailField("이메일 주소", max_length=100, unique=True)
-    password = models.CharField("비밀번호", max_length=60)
+    password = models.CharField("비밀번호", max_length=200)
     fullname = models.CharField("이름", max_length=20)
     join_date = models.DateTimeField("가입일", auto_now_add=True)
 
-    # is_active 가 False 일 경우 계정이 비활성화됨
+    # # is_active 가 False 일 경우 계정이 비활성화됨, 로그인 결정
     is_active = models.BooleanField(default=True)
 
     # is_staff 에서 해당 값 사용, admin 페이지 접근 권한
     is_admin = models.BooleanField(default=False)
 
-    # id로 사용 할 필드 지정. USERNAME_FILED : 어떤 필드를 ID 로 사용할 것인지 설정.
-    # 로그인 시 USERNAME_FIELD에 설정 된 필드와 password가 사용된다.
+    # # id로 사용 할 필드 지정. USERNAME_FILED : 어떤 필드를 ID 로 사용할 것인지 설정.
+    # # 로그인 시 USERNAME_FIELD에 설정 된 필드와 password가 사용된다.
     USERNAME_FIELD = 'username'
-
-    # user를 생성할 때 입력받은 필드 지정
+    #
+    # # user를 생성할 때 입력받은 필드 지정
     REQUIRED_FIELDS = []
-
+    #
     objects = UserManager()  # custom user 생성 시 필요
-
-    def __str__(self):
-        return f'{self.username} / {self.email}'
 
     # 로그인 사용자의 특정 테이블의 crud 권한을 설정, perm table의 crud 권한이 들어간다.
     # admin 일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
@@ -60,10 +58,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
-    # admin 권한 설정
+    # # admin 권한 설정
     @property
     def is_staff(self):
         return self.is_admin
+
+    def __str__(self):
+        return f'{self.username} / {self.email}'
 
 
 class UserProfile(models.Model):
--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.db.models import F
 from django.shortcuts import render
 from django.conf import settings
 
@@ -5,8 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 
-# Create your views here.
-from user.models import Hobby
+from user.models import Hobby, UserProfile, User
 
 
 class MyGoodPermission(permissions.BasePermission):
@@ -22,15 +23,56 @@ class UserApiView(APIView):
 
     def get(self, request):
         # 글로벌 settings의 auth_user_model
-        print(settings.AUTH_USER_MODEL)
+        print("settings.AUTH_USER_MODEL: ", settings.AUTH_USER_MODEL)
+        # 정참조
+        '''
+        user_profile = UserProfile.objects.get(user=1)
+        print("정참조")
+        print("user_profile.user: ", user_profile.user)
+        print("user_profile.hobby: ", user_profile.hobby)
+        '''
 
-        # Hobby 조회
+        # 역참조
+        print("역참조")
+        '''
+        hobby = Hobby.objects.get(id=1)
+        hobby.userprofile_set
+        '''
 
-        # DoesNotExist 핸들링
-        try:
-            hobby = Hobby.objects.get(id=500)
-        except Hobby.DoesNotExist:
-            # object가 존재하지 않을 때 이벤트
-            return Response({"error": "오브젝트가 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)  # event 발생 시 나타낼 status 직접 지정도 가능하다
+        user = User.objects.get(id=1)
 
+        hobby_s = user.userprofile.hobby.all()
+        for hobby in hobby_s:
+            # exclde : 매칭 된 쿼리만 제외, filter와 반대
+            # annotate : 필드 이름을 변경해주기 위해 사용, 이 외에도 원하는 필드를 추가하는 등 다양하게 활용 가능
+            # values / values_list : 지정한 필드만 리턴 할 수 있음. values는 dict로 return, values_list는 tuple로 ruturn
+            # F() : 객체에 해당되는 쿼리를 생성함
+            hobby_members = hobby.userprofile_set.exclude(user=user).annotate(username=F('user__username')).values_list("username", flat=True)
+            hobby_members = list(hobby_members)
+
+            print(f'hobby : {hobby.name} / 역참조 hobby members : {hobby_members}')
+
+
+        #
+        #     # break
+        # # Hobby 조회
+        # # DoesNotExist 핸들링
+        # try:
+        #     hobby = Hobby.objects.get(id=5)
+        #     print(hobby)
+        # except Hobby.DoesNotExist:
+        #     # object가 존재하지 않을 때 이벤트
+        #     return Response({"error": "오브젝트가 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)  # event 발생 시 나타낼 status 직접 지정도 가능하다
         return Response({"message": "get success!!"})
+
+
+    def post(self, request):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return Response({"message": "로그인 성공!"}, status=status.HTTP_200_OK)
