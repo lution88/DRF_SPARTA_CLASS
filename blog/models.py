52f5be1017e157cef48378cdd3af7b7bea96345a
lution88 <lution88@gmail.com>
2022-06-17 16:38:45 +0900

    로그인 추가, 유저정보 추가

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,15 @@ class Category(models.Model):
     name = models.CharField("카테고리 이름", max_length=100)
     content = models.TextField("카테고리 설명")
 
+    def __str__(self):
+        return f'{self.name} / {self.content}'
+
 
 class Article(models.Model):
     author = models.ForeignKey(Users, verbose_name="작성자", on_delete=models.CASCADE)
     title = models.CharField("글 제목", max_length=100)
     category = models.ManyToManyField(Category, verbose_name="카테고리")
     content = models.TextField("글 내용")
+
+    def __str__(self):
+        return f'{self.title} / {self.category.}'
--- user_homework/views.py
+++ user_homework/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Users, UserProfile
+from blog.models import Category, Article
+
+
+class UserHomeworkApiView(APIView):
+    def get(self, request):
+        user = request.user
+
+        print(f"유저 아이디: {user.username}")
+        print(f"유저 이메일: {user.email}")
+        print(f"유저 이 름: {user.fullname}")
+
+        articles = user.article_set.all()
+        print(articles)
+        # print(f"유저 게시글: {articles}")
+        return Response({"message":'유저정보'})
+
+    def post(self, request):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response({"error": "로그인 실패! 유저정보를 확인하세요."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return Response({"message": "로그인 성공"}, status=status.HTTP_200_OK)
