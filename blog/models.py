7a22124da3408ff17479abceb4ca47327b5d75d8
lution88 <lution88@gmail.com>
2022-06-17 12:32:33 +0900

    blog앱 Category, Article model 생성

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from user_homework.models import Users
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField("카테고리 이름", max_length=100)
     content = models.TextField("카테고리 설명")
 
+
+class Article(models.Model):
+    author = models.ForeignKey(Users, verbose_name="작성자", on_delete=models.CASCADE)
+    title = models.CharField("글 제목", max_length=100)
+    category = models.ManyToManyField(Category, verbose_name="카테고리")
+    content = models.TextField("글 내용")
