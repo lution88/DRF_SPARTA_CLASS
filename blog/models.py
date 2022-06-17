5f7b583a18bcb6c14eeabbe42b13c0601b10ddc8
lution88 <lution88@gmail.com>
2022-06-17 11:42:16 +0900

    blog앱 Category모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField("카테고리 이름", max_length=100)
+    content = models.TextField("카테고리 설명")
+
