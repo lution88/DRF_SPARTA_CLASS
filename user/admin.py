94950f0f7d6f0b11ff35b4861dd121670808ebd4
lution88 <lution88@gmail.com>
2022-06-18 22:14:33 +0900

    UserAdmin 꾸미기-list_display, list_display_link, list_filter, search_fields, fieldsets, inlines, readonly_fileds

--- user/admin.py
+++ user/admin.py
@@ -12,9 +12,35 @@ class HobbyAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfileModel
+
+    # def formfield_for_manytomany(self, db_field, request, **kwargs):
+    #     if db_field.name == 'hobby':
+    #         kwargs['queryset'] = HobbyModel.objects.filter(id__lte=7)
+    #
+    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id","username","fullname","email"] # 사용자 목록에 보여질 필드 지정.
+    list_display_links = ("id", "username", ) # 상세 페이지로 들어갈 수 있는 필드 지정.
+    list_filter = ("fullname", ) # 필터를 걸 수 있는 필드 생성.
+    search_fields = ("fullname", "username", ) # 검색에 사용될 필드 지정.
+    readonly_fields = ("username", "join_date", ) # 상세페이지에서 읽기전용필드를 설정
+    fieldsets = (
+        ("info", {'fields': ('username', 'fullname', 'join_date')}),
+        ('permissions', {'fields': ('is_admin', 'is_active', )}),
+    ) # 상세페이지에서 필드를 분류하는데 사용.
+
+    inlines = (
+        UserProfileInline,
+    )
+
+
 admin.site.register(HobbyModel, HobbyAdmin)
 admin.site.register(UserProfileModel)
-admin.site.register(UserModel)
+admin.site.register(UserModel, UserAdmin)
 
 # unregister
 admin.site.unregister(Group)
