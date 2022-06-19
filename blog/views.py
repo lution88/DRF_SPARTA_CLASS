b9ec944ccdf8d64b703393f36b142a832d8eaa3f
lution88 <lution88@gmail.com>
2022-06-19 15:32:32 +0900

    사용자가 작성한 게시글을 user의 serializer data에 포함시키기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from blog.serializers import BlogUserSerializer
+
+
+class UserAPIView(APIView):
+
+    def get(self, request):
+        user = request.user
+        return Response(BlogUserSerializer(user).data, status=status.HTTP_200_OK)
+
+
