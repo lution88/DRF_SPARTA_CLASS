6edbc078e962165c1a4e93cde821203c882e3111
lution88 <lution88@gmail.com>
2022-07-01 15:17:32 +0900

    add event model set media_root, url

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField("제품명", max_length=50)
+    thumbnail = models.ImageField("썸네일", upload_to="thumbnail", height_field=None, width_field=None, max_length=None)
+    description = models.TextField("제품 설명")
+    dt_created = models.DateTimeField("등록 일자", auto_now_add=True)
+    exposure_start_date = models.DateField("노출 시작일")
+    exposure_end_date = models.DateField("노출 종료일")
+    is_active = models.BooleanField("활성화 여부")
+
+    def __str__(self):
+        return f'{self.title} 이벤트 입니다.'
